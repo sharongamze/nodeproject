d2c312af5ed83a2d310527ed428fac28f24bef5f
sharongamze <shargamze@gmail.com>
2021-09-13 16:34:02 +0300

    first commit

--- routes/index.js
+++ routes/index.js
@@ -69,28 +69,12 @@ app.set('view engine', 'handlebars');
 
 
 router.get('/get_report',function(req, res, next) {
-    // const month = 1;
-    // const year = 1990;
-    // const fromDate = new Date(year, month-1, 1);
-    // const toDate = new Date(fromDate.getFullYear(), fromDate.getMonth() + 1, 0);
-    // const condition = { date:  { "$gte": fromDate.toString(), "$lte" : toDate.toString() } , User: req.session.user};
-    // console.log(fromDate.toString())
-    // console.log(toDate.toString())
-    // Cost.find(condition)
-    //     .then(function(doc) {
-    //         var category= [];
-    //         doc.forEach(element => category.push(element.category));
-    //         console.log(category);
-    //         res.render('report.hbs', {items: doc, category:category});
-    //     });
     res.render('report.hbs')
-
 });
 
 router.post('/test/submit',function(req, res, next) {
   var date= new Date(req.body.month)
-  var month_input = date.getMonth()
-  const month = month_input;
+  const month = date.getMonth();
   const year = date.getFullYear();
   const fromDate = new Date(year, month, 1);
   const toDate = new Date(fromDate.getFullYear(), fromDate.getMonth() + 1, 0);
