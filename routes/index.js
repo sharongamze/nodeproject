bf75819d871199b24152c0d7a85f887b157c1a84
sharongamze <shargamze@gmail.com>
2021-09-16 14:36:30 +0300

    first commit

--- routes/index.js
+++ routes/index.js
@@ -71,7 +71,7 @@ router.post('/insert', function(req, res, next) {
                 };
                 var new_user_change = new changes(userchange);
                 await new_user_change.save();
-                id.forEach(element => {if(element === data_cost){var index = id.indexOf(element);id.splice(index,1)}});
+                id.forEach(element => {if(element === data_cost._id){var index = id.indexOf(element);id.splice(index,1)}});
                 await changes.findByIdAndUpdate(new_user_change, {$push: {Cost: id}})
             }
 
