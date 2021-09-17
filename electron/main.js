b9f70cd6daa76983465fd0d3e07ff2e44df8d4d7
sharongamze <shargamze@gmail.com>
2021-09-17 19:36:55 +0300

    adding electron- desktop application bonus

---
+++ electron/main.js
@@ -0,0 +1,16 @@
+//we use this file to open desktop application - Electron
+// to run this desktop  on terminal - npm run launch
+const { BrowserWindow, app } = require('electron');
+require('../app.js');
+
+let mainWindow = null;
+
+function main() {
+    mainWindow = new BrowserWindow()
+    mainWindow.loadURL(`http://localhost:3000/`)
+    mainWindow.on('close', event => {
+        mainWindow = null
+    })
+}
+
+app.on('ready', main);
