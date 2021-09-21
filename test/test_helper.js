47e65a4b361907bb23253557130b8d5463c5c6cf
sharongamze <shargamze@gmail.com>
2021-09-21 21:57:47 +0300

    adding mocha tests

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,7 +3,8 @@ const config = require('config');
 const Cost= require('../model/cost');
 mongoose.Promise = global.Promise;
 
-
+//new data base- Test for mocha tests
+// to run test- npm run test
 
 mongoose.connect(`mongodb+srv://${config.get('db.user')}:${config.get('db.password')}@cluster0.6p3gd.mongodb.net/Test?retryWrites=true&w=majority`).then(() => console.log('Connected to MongoDB Successfully'))
 mongoose.connection
